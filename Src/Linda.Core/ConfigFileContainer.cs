0cabdae675bfd7bacf0df9454f3039b764359ff7
Airat93 <jastis1993@yandex.ru>
2013-07-25 16:43:34 +0400

    Add ConfigFileContainer

---
+++ Src/Linda.Core/ConfigFileContainer.cs
@@ -0,0 +1,41 @@
+﻿namespace Linda.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class ConfigFileContainer : IConfigSourceProvider
+    {
+        public List<ConfigGroup> GetCs(string path)
+        {
+            var newConfFolders = new List<ConfigGroup>();
+
+            var dir = new DirectoryInfo(path);
+
+            while (dir != null)
+            {
+                var cg = new ConfigGroup();
+
+                if (Directory.Exists(path))
+                {
+                    var yamlFiles = Directory.GetFiles(path, "*.yaml");
+
+                    foreach (var yamlFile in yamlFiles)
+                    {
+                        cg.AddConfigSource(new ConfigSource(yamlFile));
+                    }
+
+                    newConfFolders.Add(cg);
+                }
+
+                dir = dir.Parent;
+            }
+
+            newConfFolders.Reverse();
+
+            return newConfFolders;
+        }
+    }
+}
