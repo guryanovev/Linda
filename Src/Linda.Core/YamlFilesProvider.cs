386e0e0d2bec36abd39f79db9aeb9c8e9957f49d
k1nval <stasyan93@bk.ru>
2013-07-25 15:49:17 +0400

    yaml provider

---
+++ Src/Linda.Core/YamlFilesProvider.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Linda.Core
+{
+    using System.IO;
+    using System.Text;
+
+    public static class YamlFilesProvider
+    {
+        public static string GetFileContent(string path)
+        {
+            if (File.Exists(path))
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+
+            throw new FileNotFoundException();
+        }
+
+        public static string GetFolderConfigContent(string path)
+        {
+            var files = Directory.GetFiles(path, "*.yaml");
+
+            var folderContent = new StringBuilder();
+
+            foreach (var file in files)
+            {
+                folderContent.AppendLine(GetFileContent(file));
+            }
+
+            return folderContent.ToString();
+        }
+    }
+}
