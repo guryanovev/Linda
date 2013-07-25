e32bc70f277405b1aa615036a883593e780dc2cf
Airat93 <jastis1993@yandex.ru>
2013-07-25 16:21:30 +0400

    new ConfigFolderContainer

--- Src/Linda.Core/ConfigFolderContainer.cs
+++ Src/Linda.Core/ConfigFolderContainer.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
 
@@ -9,7 +10,30 @@
     {
         public List<ConfigGroup> GetCs(string path)
         {
+            var newConfFolders = new List<ConfigGroup>();
 
+            var dir = new DirectoryInfo(path);
+
+            while (dir != null)
+            {
+                var cg = new ConfigGroup();
+
+                if (Directory.Exists(dir.FullName + "/config/"))
+                {
+                    var yamlFiles = dir.GetFiles("*.yaml");
+
+                    foreach (var yamlFile in yamlFiles)
+                    {
+                        cg.AddConfigSource(new ConfigSource(yamlFile.FullName));
+                    }
+                }
+
+                newConfFolders.Add(cg);
+
+                dir = dir.Parent;
+            }
+
+            return newConfFolders;
         }
     }
 }
