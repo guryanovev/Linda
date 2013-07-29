f52478e31a15a880515c5e0e95f80d2ab4208665
k1nval <stasyan93@bk.ru>
2013-07-29 17:24:57 +0400

    Fixed Test

--- Src/Linda.Core.AcceptanceTests/AllTypeTests.cs
+++ Src/Linda.Core.AcceptanceTests/AllTypeTests.cs
@@ -9,20 +9,29 @@ namespace Linda.Core.AcceptanceTests
     class AllTypeTests : TestsBase
     {
         [Test]
-        [Culture("de-DE")]
+        [Culture("")]
         public void Test_AllTypeConfig()
         {
             CreateFile(
                 "config/config1.yml",
-@"ByteArrayProp:
-    - 0
-    - 1
-    - 2");
+@"BoolProp: True
+IntProp: 666
+StringProp: Hello World
+DateProp: 2013-07-27
+DoubleProp: 1.333
+FloatProp: 2.666");
+
 
             var config = LoadConfig<AllTypeConfig>();
 
-            
-            //Assert.That(config.DateProp, Is.EqualTo(new DateTime(2013, 7, 27)));
+            Assert.That(config, Is.Not.Null);
+            Assert.That(config.BoolProp, Is.EqualTo(true));
+            Assert.That(config.IntProp, Is.EqualTo(666));
+            Assert.That(config.FloatProp, Is.EqualTo((float)2.666));
+            Assert.That(config.StringProp, Is.EqualTo("Hello World"));
+            Assert.That(config.DoubleProp, Is.EqualTo(1.333));
+            Assert.That(config.DateProp, Is.EqualTo(new DateTime(2013, 7, 27)));
+
         }
 
         [Test]
