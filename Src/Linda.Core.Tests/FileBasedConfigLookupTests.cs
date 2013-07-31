18b9d5e7b2376738b6c7a036cfdae9fde700e3ea
Airat93 <jastis1993@yandex.ru>
2013-07-31 14:44:23 +0400

    Change tests

--- Src/Linda.Core.Tests/FileBasedConfigLookupTests.cs
+++ Src/Linda.Core.Tests/FileBasedConfigLookupTests.cs
@@ -13,16 +13,15 @@
         [Test]
         public void SimpleTest()
         {
-            var filesSystem = new Mock<IFilesSystem>();
+            var filesSystem =
+                Mock.Of<IFilesSystem>(
+                    f =>
+                    f.Exists("path") &&
+                    f.GetFiles("path") == new List<string> { "path/config.yml" }
+                    && f.GetFileContent("path/config.yml") == "Foo: fooValue"
+                    && f.GetParentDirectory("path") == (string)null);
 
-            filesSystem.Setup(f => f.Exists("path")).Returns(true);
-            filesSystem.Setup(f => f.GetFiles("path")).Returns(() => new List<string> { "path/config.yml" });
-            filesSystem.Setup(f => f.GetFileContent("path/config.yml")).Returns("Foo: fooValue");
-            filesSystem.Setup(f => f.GetParentDirectory("path")).Returns((string)null);
-
-            var fS = filesSystem.Object;
-
-            var fileBasedConfigLookup = new FileBasedConfigLookup(fS);
+            var fileBasedConfigLookup = new FileBasedConfigLookup(filesSystem);
 
             var configGroups = fileBasedConfigLookup.GetConfigGroups("path").ToList();
 
@@ -39,17 +38,24 @@
         [Test]
         public void SomeFileTest()
         {
-            var filesSystem = new Mock<IFilesSystem>();
+            /*var filesSystem = new Mock<IFilesSystem>();
 
             filesSystem.Setup(fs => fs.Exists("path")).Returns(true);
             filesSystem.Setup(fs => fs.GetFiles("path")).Returns(() => new List<string> { "path/foo.yml", "path/bar.yml" });
             filesSystem.Setup(fs => fs.GetFileContent("path/foo.yml")).Returns("Foo: fooValue");
             filesSystem.Setup(fs => fs.GetFileContent("path/bar.yml")).Returns("Bar: barValue");
-            filesSystem.Setup(fs => fs.GetParentDirectory("path")).Returns((string)null);
+            filesSystem.Setup(fs => fs.GetParentDirectory("path")).Returns((string)null);*/
 
-            var fS = filesSystem.Object;
+            var filesSystem =
+                Mock.Of<IFilesSystem>(
+                    f =>
+                    f.Exists("path") 
+                    && f.GetFiles("path") == new List<string> { "path/foo.yml", "path/bar.yml" }
+                    && f.GetFileContent("path/foo.yml") == "Foo: fooValue"
+                    && f.GetFileContent("path/bar.yml") == "Bar: barValue"
+                    && f.GetParentDirectory("path") == (string)null);
 
-            var fileBasedConfigLookup = new FileBasedConfigLookup(fS);
+            var fileBasedConfigLookup = new FileBasedConfigLookup(filesSystem);
 
             var configGroups = fileBasedConfigLookup.GetConfigGroups("path").ToList();
 
@@ -66,30 +72,23 @@
         [Test]
         public void SomeFileTestWithHierarchy()
         {
-            var filesSystem = new Mock<IFilesSystem>();
+            var filesSystem =
+                Mock.Of<IFilesSystem>(
+                    f =>
+                    f.Exists("path") && f.Exists("under path") && f.Exists("under path")
+                    && f.Exists("parent under path") && f.GetFiles("path") == new List<string> { "path/config1.yml" }
+                    && f.GetFiles("under path")
+                       == new List<string> { "under path/config2.yml", "under path/config3.yml" }
+                    && f.GetFiles("parent under path") == new List<string> { "parent under path/config4.yml" }
+                    && f.GetFileContent("path/config1.yml") == "Foo: fooValue"
+                    && f.GetFileContent("under path/config2.yml") == "Bar: barValue"
+                    && f.GetFileContent("under path/config3.yml") == "Baz: bazValue"
+                    && f.GetFileContent("parent under path/config4.yml") == "FooBar: fooBarValue"
+                    && f.GetParentDirectory("path") == "under path"
+                    && f.GetParentDirectory("under path") == "parent under path"
+                    && f.GetParentDirectory("parent under path") == (string)null);
 
-            filesSystem.Setup(fs => fs.Exists("path")).Returns(true);
-            filesSystem.Setup(fs => fs.Exists("under path")).Returns(true);
-            filesSystem.Setup(fs => fs.Exists("parent under path")).Returns(true);
-
-            filesSystem.Setup(fs => fs.GetFiles("path")).Returns(() => new List<string> { "path/config1.yml" });
-            filesSystem.Setup(fs => fs.GetFiles("under path"))
-                       .Returns(() => new List<string> { "under path/config2.yml", "under path/config3.yml" });
-            filesSystem.Setup(fs => fs.GetFiles("parent under path"))
-                       .Returns(() => new List<string> { "parent under path/config4.yml" });
-
-            filesSystem.Setup(fs => fs.GetFileContent("path/config1.yml")).Returns("Foo: fooValue");
-            filesSystem.Setup(fs => fs.GetFileContent("under path/config2.yml")).Returns("Bar: barValue");
-            filesSystem.Setup(fs => fs.GetFileContent("under path/config3.yml")).Returns("Baz: bazValue");
-            filesSystem.Setup(fs => fs.GetFileContent("parent under path/config4.yml")).Returns("FooBar: fooBarValue");
-
-            filesSystem.Setup(fs => fs.GetParentDirectory("path")).Returns(() => "under path");
-            filesSystem.Setup(fs => fs.GetParentDirectory("under path")).Returns(() => "parent under path");
-            filesSystem.Setup(fs => fs.GetParentDirectory("parent under path")).Returns((string)null);
-
-            var fS = filesSystem.Object;
-
-            var fileBasedConfigLookup = new FileBasedConfigLookup(fS);
+            var fileBasedConfigLookup = new FileBasedConfigLookup(filesSystem);
 
             var configGroups = fileBasedConfigLookup.GetConfigGroups("path").ToList();
 
