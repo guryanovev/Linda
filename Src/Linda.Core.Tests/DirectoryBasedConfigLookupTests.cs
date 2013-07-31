c363ce38b66c2ea563ead0e4617c35669fcebba5
Airat93 <jastis1993@yandex.ru>
2013-07-31 12:15:45 +0400

    Add new tests

---
+++ Src/Linda.Core.Tests/DirectoryBasedConfigLookupTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linda.Core.Tests
+{
+    class DirectoryBasedConfigLookupTests
+    {
+    }
+}
