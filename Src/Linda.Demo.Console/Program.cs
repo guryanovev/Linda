eceb8e4bbf6e5889d58f50095e4c296e28034555
Airat93 <jastis1993@yandex.ru>
2013-08-02 12:26:17 +0400

    Demo.Console Edit

--- Src/Linda.Demo.Console/Program.cs
+++ Src/Linda.Demo.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace Linda.Demo.Console
 {
     using System;
+    using System.Collections.Generic;
 
     using Linda.Core;
     using Linda.Core.Lookup;
@@ -22,9 +23,34 @@
         public StartUp Startup { get; set; }
     }
 
+    public class AnotherConfig
+    {
+        public string Foo { get; set; }
+
+        public List<Struct> StructProp { get; set; }
+
+        public struct Struct
+        {
+            public string Bar { get; set; }
+
+            public string Baz { get; set; }
+        }
+    }
+
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
+        {
+            Console.WriteLine("Work with files");
+            FileBasedConfig();
+
+            Console.WriteLine("\n\nWork with directory");
+            DirectoryBasedConfig();
+
+            Console.ReadKey();
+        }
+
+        private static void FileBasedConfig()
         {
             var lookup = new FileBasedConfigLookup { SearchPattern = "App.yml" };
 
@@ -32,11 +58,25 @@
 
             var config = manager.GetConfig<Configuration>();
 
-            Console.WriteLine(config.Startup.SupportedRuntime.Sku);
+            Console.WriteLine("Sku = {0}", config.Startup.SupportedRuntime.Sku);
 
-            Console.WriteLine(config.Startup.SupportedRuntime.Version);
+            Console.WriteLine("Version = {0}", config.Startup.SupportedRuntime.Version);
+        }
 
-            // todo
+        private static void DirectoryBasedConfig()
+        {
+            var lookup = new DirectoryBasedConfigLookup { DirectoryName = "config", SearchPattern = "*.yml" };
+
+            var manager = new DefaultConfigManager(lookup);
+
+            var config = manager.GetConfig<AnotherConfig>();
+
+            Console.WriteLine("Foo: {0}", config.Foo);
+
+            foreach (var structP in config.StructProp)
+            {
+                Console.WriteLine("StructProperty:\n\tBaz: {0}\n\tBar: {1}\n", structP.Bar, structP.Baz);
+            }
         }
     }
 }
