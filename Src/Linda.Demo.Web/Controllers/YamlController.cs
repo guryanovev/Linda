f1f6c33b7a9404b003406da05083959faa6f4b45
Airat93 <jastis1993@yandex.ru>
2013-08-06 15:50:27 +0400

    demo projects move to .net 4.0

--- Src/Linda.Demo.Web/Controllers/YamlController.cs
+++ Src/Linda.Demo.Web/Controllers/YamlController.cs
@@ -1,9 +1,5 @@
 ﻿namespace Linda.Demo.Web.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Web;
     using System.Web.Mvc;
 
     using Linda.Core;
