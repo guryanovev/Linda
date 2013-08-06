dd5daa73765f9b082b8544f92e97bb3e27652d2b
Airat93 <jastis1993@yandex.ru>
2013-08-06 17:34:48 +0400

    ...

--- Src/Linda.Demo.Web/Global.asax.cs
+++ Src/Linda.Demo.Web/Global.asax.cs
@@ -17,7 +17,6 @@
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
             //WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
