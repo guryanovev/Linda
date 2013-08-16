b8b8f894407847818e4aeb8784becad25d0c41a0
Airat93 <jastis1993@yandex.ru>
2013-08-16 11:21:29 +0400

    Edit build

--- Src/RunBuild.bat
+++ Src/RunBuild.bat
@@ -1,2 +1,2 @@
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Build.proj /p:TargetFrameworkVersion=v4.0
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Build.proj /p:TargetFrameworkVersion=v4.0 /target:BuildSolution;RunTestsOnLocalMachine;CopyToArtefacts
 pause
