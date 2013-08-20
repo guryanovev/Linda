c11e4a1b7157f82c07b8a95b4a064dfa47b498e0
Airat93 <jastis1993@yandex.ru>
2013-08-20 17:46:56 +0400

    fix Demo.Web.csproj

--- Src/RunBuild.bat
+++ Src/RunBuild.bat
@@ -1,2 +1,2 @@
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Build.proj /p:TargetFrameworkVersion=v4.0 /target:BuildSolution;RunTestsOnLocalMachine;CopyToArtifacts
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe Build.proj /p:TargetFrameworkVersion=v4.0 /target:BeforeBuild;BuildSolution;RunTestsOnLocalMachine;CopyToArtifacts
 pause
