eb577d8d33c3e6a95f1dcb13617504c158b9437e
Airat93 <jastis1993@yandex.ru>
2013-08-21 16:04:41 +0400

    Add auto-increment version

--- Src/Properties/VersionInfo.cs
+++ Src/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyFileVersion("1.0.")]
-[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
 
 
